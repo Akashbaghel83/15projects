5e634b7dd0f7fe3c81c13c6b843a2a24196fc10d
Akashbaghel83 <akashbaghelji83@gmail.com>
2024-12-26 20:53:48 +0530

    cardjs

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-var btn = document.querySelector("button");
-var bulb = document.querySelector(".bulb");
-var flag = 0;
+var users = [
+    {
+        name: 'Akash',
+        age: 21,
+        city: 'Bhopal'
+    },
+    {
+        name: 'Ravi',
+        age: 22,
+        city: 'Bihar'
+    },
+    {
+        name: 'Pardhi',
+        age:19,
+        city: 'Balaghat'
+    }
+]
+var sum = '';
+users.forEach(function(item){
+    sum += `<div class="card">
+        <h1>${item.name}</h1>
+        <h4>${item.age},${item.city}</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+    </div>`;
+})
 
-btn.addEventListener("click", function () {
-  if (flag === 0) {
-    btn.textContent = "OFF";
-    bulb.style.backgroundColor = "orange";
-    flag = 1;
-  } else {
-    btn.textContent = "ON";
-    bulb.style.backgroundColor = "gray";
-    flag = 0;
-  }
-});
+document.querySelector('body').innerHTML = sum;
