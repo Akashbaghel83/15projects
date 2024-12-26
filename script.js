d9634d2cfc5ec75de85008878c5863efc5fa9b78
Akashbaghel83 <akashbaghelji83@gmail.com>
2024-12-26 20:57:38 +0530

    Friend Request

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-var a = document.querySelector('button')
-var b = document.querySelector('h1')
-var c = document.querySelector('.real')
-var d = 0
+var h2 = document.querySelector("h2");
+var btn = document.querySelector("button");
+var flag = 0
 
-a.addEventListener('click', function(){
-    var e = setInterval(function(){
-        d++
-        b.innerHTML = d + '%'
-        c.style.width = d + '%'
-                
-    },30)
-    setTimeout(function(){
-        clearInterval(e)
-        a.style.pointerEvents = 'none'
-        a.innerHTML = "DOWNLOADED"
-        a.style.opacity = 0.4
+btn.addEventListener("click", function () {
+  if(flag === 0){
+    h2.style.color = "orange";
+    h2.innerHTML = "Request sending...";
+
+    setTimeout(function () {
+      h2.innerHTML = "Friends";
+      h2.style.color = "green";
+      btn.innerHTML = "Remove Friend";
+      flag = 1;
+    }, 1000);
+  }
+  else{
+    h2.style.color = "orange";
+    h2.innerHTML = "Removing...";
+
+    setTimeout(function () {
+      h2.innerHTML = "Stranger";
+      h2.style.color = "red";
+      btn.innerHTML = "Add Friend";
+      flag = 0;
+    }, 1000);
+  }
 
 
-    },3000)
-})
+});
