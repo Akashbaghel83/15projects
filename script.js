29a8718f3a5ebf0ab8e687f12e590e892d8ca777
Akashbaghel83 <akashbaghelji83@gmail.com>
2024-12-26 20:54:41 +0530

    custom cursor

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
-var users = [
-    {
-        name: 'Akash',
-        age: 21,
-        city: 'Bhopal'
-    },
-    {
-        name: 'Ravi',
-        age: 22,
-        city: 'Bihar'
-    },
-    {
-        name: 'Pardhi',
-        age:19,
-        city: 'Balaghat'
-    }
-]
-var sum = '';
-users.forEach(function(item){
-    sum += `<div class="card">
-        <h1>${item.name}</h1>
-        <h4>${item.age},${item.city}</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-    </div>`;
-})
+var a = document.querySelector('body')
+var b = document.querySelector('#cursor')
+var c = document.querySelector('h1')
+a.addEventListener('mousemove', function(dets){
+    b.style.top = dets.y + "px"
+    b.style.left = dets.x + "px"
 
-document.querySelector('body').innerHTML = sum;
+})
+c.addEventListener('mouseenter', function(){
+    b.style.scale = 3
+    
+})
+c.addEventListener('mouseleave', function(){
+    b.style.scale = 1
+
+
+})
