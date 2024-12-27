bbece1581b17e09d8a693ab183d2e8c005ac88a7
Akashbaghel83 <akashbaghelji83@gmail.com>
2024-12-27 08:27:55 +0530

    Team selection

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-var a = document.querySelector('button')
-var b = document.querySelector('body')
-images = ['https://cdn-icons-png.flaticon.com/512/785/785104.png',
-    'https://static.vecteezy.com/system/resources/thumbnails/028/283/955/small/cartoon-insect-aigenerative-png.png',
-    'https://i.pinimg.com/originals/a2/2c/7b/a22c7bdbdf4df2de65f20272b91568fe.png']
-a.addEventListener('click', function(){
-    var r = Math.trunc(Math.random()*3)
-    console.log(r);
-    vx = Math.random()*100
-    vy = Math.random()*100
-    vz = Math.random()*360
-    var c = document.createElement('img')
-    c.setAttribute('src', images[r])
-    c.style.height = '50px'
-    c.style.position = 'absolute'
-    c.style.top = vx + '%'
-    c.style.left = vy + '%'
-    c.style.rotate = vz + 'deg'
-    b.appendChild(c)
-})
+var teams = ["CSK", "MI", "KKR", "GT", "SRH", "RR", "LSG", "PBKS", "DC"];
+
+var btn = document.querySelector("button");
+var txt = document.querySelector("h1");
+
+btn.addEventListener("click", function () {
+  var num = Math.floor(Math.random() * teams.length);
+
+  txt.innerHTML = teams[num];
+});
