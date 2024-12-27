99cf235db90102fb184d1066dee32ceb77e59ac6
Akashbaghel83 <akashbaghelji83@gmail.com>
2024-12-27 08:30:34 +0530

    Multiple selection

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-let img = document.querySelector("img");
-let imgX = 0;
-let imgY = 0;
+var a = document.querySelectorAll('button')
+var b = document.querySelector('body')
+a.forEach(function(elem){
+    elem.addEventListener('click', function(){
+        b.style.backgroundColor = elem.innerHTML
 
-document.addEventListener("keydown", function (dets) {
-  if (dets.code == "ArrowRight"  && imgX < 87) {
-    imgX++;
-    img.style.left = imgX + "%";
-  } else if (dets.code == "ArrowLeft" && imgX >= 0) {
-    imgX--;
-    img.style.left = imgX + "%";
-  } else if (dets.code == "ArrowUp"  && imgY >=0) {
-    imgY--;
-    img.style.top = imgY + "%";
-  } else if (dets.code == "ArrowDown" && imgY <70 ) {
-    imgY++;
-    img.style.top = imgY + "%";
-  }
-});
+        
+
+    })
+    
+})
