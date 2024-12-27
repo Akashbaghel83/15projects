3bb5d4a1acc85f21695cce6a930d5d2c141eab3b
Akashbaghel83 <akashbaghelji83@gmail.com>
2024-12-27 08:25:44 +0530

    image swap

--- script.js
+++ script.js
@@ -1,30 +1,9 @@
-var h2 = document.querySelector("h2");
-var btn = document.querySelector("button");
-var flag = 0
-
-btn.addEventListener("click", function () {
-  if(flag === 0){
-    h2.style.color = "orange";
-    h2.innerHTML = "Request sending...";
-
-    setTimeout(function () {
-      h2.innerHTML = "Friends";
-      h2.style.color = "green";
-      btn.innerHTML = "Remove Friend";
-      flag = 1;
-    }, 1000);
-  }
-  else{
-    h2.style.color = "orange";
-    h2.innerHTML = "Removing...";
-
-    setTimeout(function () {
-      h2.innerHTML = "Stranger";
-      h2.style.color = "red";
-      btn.innerHTML = "Add Friend";
-      flag = 0;
-    }, 1000);
-  }
-
-
-});
+var a = document.querySelector('#img1')
+var b = document.querySelector('#img2')
+var c = document.querySelector('button')
+c.addEventListener('click', function(){
+    var d = a.getAttribute('src')
+    var e = b.getAttribute('src')
+    a.setAttribute('src',e)
+    b.setAttribute('src',d)    
+})
